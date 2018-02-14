7705051fb9d206086a7b26eeb2d18a4119857c7d
Ben Fernandez <bfernandez19125@columbushs.com>
2018-02-14 14:30:11 -0500

    message

---
+++ src/learning/lab/LearningLab.java
@@ -0,0 +1,50 @@
+package learning.lab;
+
+import java.util.ArrayList;
+
+
+public class LearningLab {
+
+    public static ArrayList<String> band = new ArrayList<String>();
+    public static void main(String[] args) {
+    /*
+    1)band.add("Lindsey");
+    band.add("Mick");
+    band.add("Stevie");
+    band.add("Danny");
+    band.set(3, "Christine");
+        System.out.println(band);  
+    
+    2)band.add("Lindsey");
+    band.add("Mick");
+    band.add("Stevie");
+    band.add("Peter");
+    band.remove(3);
+    band.add(2, "Christine");
+        System.out.println(band);
+    
+    3)band.add("Lindsey");
+    band.add("Mick");
+    band.add("Stevie");
+    band.add("Dave");
+    band.add("Christine");
+    band.add(band.remove(2));
+    band.set(0,band.get(4));
+        System.out.println(band);
+    
+    4)C
+    
+    5)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    */
+    }
+    
+}
